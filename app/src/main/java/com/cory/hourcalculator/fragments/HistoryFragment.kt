4536cf75ff31b3698603815c1864fd9811c11254
corylowry12 <corylowry12@gmail.com>
2021-11-06 16:31:13 -0500

    Added a fab to scroll back to the top

--- app/src/main/java/com/cory/hourcalculator/fragments/HistoryFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/HistoryFragment.kt
@@ -1,17 +1,22 @@
 package com.cory.hourcalculator.fragments
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import android.widget.ListView
 import android.widget.Toast
 import com.cory.hourcalculator.MainActivity
 import com.cory.hourcalculator.R
 import com.cory.hourcalculator.adapters.CustomAdapter
+import com.cory.hourcalculator.classes.AccentColor
 import com.cory.hourcalculator.database.DBHelper
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HistoryFragment : Fragment() {
 
@@ -21,12 +26,49 @@ class HistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val accentColor = AccentColor(requireContext())
+        when {
+            accentColor.loadAccent() == 0 -> {
+                activity?.theme?.applyStyle(R.style.teal_accent, true)
+            }
+            accentColor.loadAccent() == 1 -> {
+                activity?.theme?.applyStyle(R.style.pink_accent, true)
+            }
+            accentColor.loadAccent() == 2 -> {
+                activity?.theme?.applyStyle(R.style.orange_accent, true)
+            }
+            accentColor.loadAccent() == 3 -> {
+                activity?.theme?.applyStyle(R.style.red_accent, true)
+            }
+            accentColor.loadAccent() == 4 -> {
+                activity?.theme?.applyStyle(R.style.system_accent, true)
+            }
+        }
         return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val accentColor = AccentColor(requireContext())
+        val floatingActionButtonHistory = activity?.findViewById<FloatingActionButton>(R.id.floatingActionButtonHistory)
+        when {
+
+            accentColor.loadAccent() == 0 -> {
+                floatingActionButtonHistory?.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.colorPrimary))
+            }
+            accentColor.loadAccent() == 1 -> {
+                floatingActionButtonHistory?.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.pinkAccent))
+            }
+            accentColor.loadAccent() == 2 -> {
+                floatingActionButtonHistory?.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.orangeAccent))
+            }
+            accentColor.loadAccent() == 3 -> {
+                floatingActionButtonHistory?.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.redAccent))
+            }
+            accentColor.loadAccent() == 4 -> {
+                floatingActionButtonHistory?.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.pixelAccent))
+            }
+        }
 
         val topAppBar = activity?.findViewById<MaterialToolbar>(R.id.materialToolBarHistory)
 
@@ -45,6 +87,26 @@ class HistoryFragment : Fragment() {
 
                 else -> false
             }
+        }
+
+        val listView = activity?.findViewById<ListView>(R.id.listView)
+        listView?.setOnScrollListener(object : AbsListView.OnScrollListener {
+            override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
+
+                if (firstVisibleItem > 0) {
+                    floatingActionButtonHistory?.show()
+                } else {
+
+                    floatingActionButtonHistory?.hide()
+                }
+            }
+
+            override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {}
+        })
+
+        floatingActionButtonHistory?.setOnClickListener {
+            //vibration(vibrationData)
+            listView?.smoothScrollToPosition(0)
         }
 
         loadIntoList()
