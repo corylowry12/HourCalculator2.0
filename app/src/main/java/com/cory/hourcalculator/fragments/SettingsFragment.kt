b32fa93ffb81e72d4bcc661271b56a251bd95fc4
corylowry12 <corylowry12@gmail.com>
2021-11-06 16:28:25 -0500

    Added theming

--- app/src/main/java/com/cory/hourcalculator/fragments/SettingsFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/SettingsFragment.kt
@@ -1,11 +1,12 @@
 package com.cory.hourcalculator.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
 import com.cory.hourcalculator.R
+import com.cory.hourcalculator.classes.AccentColor
 
 class SettingsFragment : Fragment() {
 
@@ -13,7 +14,37 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val accentColor = AccentColor(requireContext())
+        when {
+            accentColor.loadAccent() == 0 -> {
+                activity?.theme?.applyStyle(R.style.teal_accent, true)
+            }
+            accentColor.loadAccent() == 1 -> {
+                activity?.theme?.applyStyle(R.style.pink_accent, true)
+            }
+            accentColor.loadAccent() == 2 -> {
+                activity?.theme?.applyStyle(R.style.orange_accent, true)
+            }
+            accentColor.loadAccent() == 3 -> {
+                activity?.theme?.applyStyle(R.style.red_accent, true)
+            }
+            accentColor.loadAccent() == 4 -> {
+                activity?.theme?.applyStyle(R.style.system_accent, true)
+            }
+        }
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val appearanceHeading = activity?.findViewById<TextView>(R.id.themeHeading)
+        appearanceHeading?.setOnClickListener {
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+
+            transaction?.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
+            transaction?.replace(R.id.fragment_container, AppearanceFragment())?.addToBackStack(null)
+            transaction?.commit()
+        }
     }
 }
