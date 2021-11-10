46b749dfe8df5c2fb67973f01eab84d66eb917fa
corylowry12 <corylowry12@gmail.com>
2021-11-10 00:12:03 -0600

    added days worked class

---
+++ app/src/main/java/com/cory/hourcalculator/classes/DaysWorkedPerWeek.kt
@@ -0,0 +1,22 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class DaysWorkedPerWeek(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    //this saves the wage amount
+    fun setDaysWorked(state: Int) {
+        val editor = sharedPreferences.edit()
+        editor.putInt("Days", state)
+        editor.apply()
+    }
+
+    // this will load wage amount
+    fun loadDaysWorked(): Int {
+        val state = sharedPreferences.getInt("Days", 7)
+        return (state)
+    }
+}
