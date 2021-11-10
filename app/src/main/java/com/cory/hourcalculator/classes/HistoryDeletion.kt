9e11f793b56aab100956e48d8d4abc1f955700a8
corylowry12 <corylowry12@gmail.com>
2021-11-10 00:11:54 -0600

    Added automatic deletion class

---
+++ app/src/main/java/com/cory/hourcalculator/classes/HistoryDeletion.kt
@@ -0,0 +1,37 @@
+package com.cory.hourcalculator.classes
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.cory.hourcalculator.database.DBHelper
+
+class HistoryDeletion(context: Context) {
+
+    private val dbHandler = DBHelper(context, null)
+
+    private val dataList = ArrayList<HashMap<String, String>>()
+
+    private val daysWorkedPerWeek = DaysWorkedPerWeek(context)
+
+    @SuppressLint("Range")
+    fun deletion(context: Context) {
+        val numberToDelete = dbHandler.getCount() - daysWorkedPerWeek.loadDaysWorked().toString().toInt()
+        dataList.clear()
+        val cursor = dbHandler.automaticDeletion(numberToDelete)
+        cursor!!.moveToFirst()
+
+        val map = HashMap<String, String>()
+        while (!cursor.isAfterLast) {
+
+            map["id"] = cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_ID))
+            map["intime"] = cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_IN))
+            map["out"] = cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_OUT))
+            map["total"] = cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_TOTAL))
+            map["day"] = cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_DAY))
+            dataList.add(map)
+
+            dbHandler.deleteRow(map["id"].toString())
+
+            cursor.moveToNext()
+        }
+    }
+}
