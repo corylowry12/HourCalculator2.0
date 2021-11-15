b69533fb2b81651b4dbdeeca0fc6b835f4ec93cd
corylowry12 <corylowry12@gmail.com>
2021-11-15 16:17:52 -0600

    Changed class path

--- app/src/main/java/com/cory/hourcalculator/database/DBHelper.kt
+++ app/src/main/java/com/cory/hourcalculator/database/DBHelper.kt
@@ -8,7 +8,7 @@ import android.database.Cursor
 import android.database.DatabaseUtils
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import com.cory.hourcalculator.SortData
+import com.cory.hourcalculator.classes.SortData
 
 class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
