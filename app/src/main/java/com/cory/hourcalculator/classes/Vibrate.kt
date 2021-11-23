9430e38a701be94aa742f59c2c5a6bf602ec8ec7
corylowry12 <corylowry12@gmail.com>
2021-11-23 08:28:38 -0600

    fixed vibration not working on android 12

--- app/src/main/java/com/cory/hourcalculator/classes/Vibrate.kt
+++ app/src/main/java/com/cory/hourcalculator/classes/Vibrate.kt
@@ -10,13 +10,27 @@ class Vibrate() {
 
     fun vibration(context: Context) {
         val vibrationData = VibrationData(context)
+
+        val vibrator: Vibrator
         if (vibrationData.loadVibrationState()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-             vibratorManager.defaultVibrator
+                val vibratorManager =
+                    context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                vibrator = vibratorManager.defaultVibrator
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        1,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
             } else {
-                val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                vibrator.vibrate(VibrationEffect.createOneShot(1, VibrationEffect.DEFAULT_AMPLITUDE))
+                vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        1,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
             }
 
         }
