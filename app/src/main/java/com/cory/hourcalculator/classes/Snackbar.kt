847d47fc5fb6d9317086d15576cb483b53bb5d87
corylowry12 <corylowry12@gmail.com>
2021-11-28 16:21:06 -0600

    Added class to show snackbar

---
+++ app/src/main/java/com/cory/hourcalculator/classes/Snackbar.kt
@@ -0,0 +1,45 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.cory.hourcalculator.MainActivity
+import com.cory.hourcalculator.R
+import com.cory.hourcalculator.database.DBHelper
+import com.cory.hourcalculator.fragments.HistoryFragment
+import com.google.android.material.snackbar.Snackbar
+
+class Snackbar() {
+
+    fun snackbar(context: Context, view: View) {
+
+        val dbHandler = DBHelper(context, null)
+
+        val undoHoursData = UndoHoursData(context)
+
+        val snackbar =
+            Snackbar.make(view, "Hour Deleted", Snackbar.LENGTH_LONG)
+                .setDuration(5000)
+                .setAnchorView(R.id.bottom_nav)
+        snackbar.setAction("UNDO") {
+            dbHandler.insertRow(
+                undoHoursData.loadInTime(),
+                undoHoursData.loadOutTime(),
+                undoHoursData.loadTotalHours(),
+                undoHoursData.loadDate(),
+                undoHoursData.loadBreakTime()
+            )
+            val runnable = Runnable {
+                (context as MainActivity).update()
+            }
+            MainActivity().runOnUiThread(runnable)
+        }
+        snackbar.setActionTextColor(
+            ContextCompat.getColorStateList(
+                context,
+                AccentColor(context).snackbarActionTextColor()
+            )
+        )
+        snackbar.show()
+    }
+}
