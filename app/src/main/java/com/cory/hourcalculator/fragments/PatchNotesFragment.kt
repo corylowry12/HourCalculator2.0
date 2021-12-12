d025b0da3c3aad655597ae84a37c9afcb21546af
corylowry12 <corylowry12@gmail.com>
2021-12-12 15:56:03 -0600

    Added more content

--- app/src/main/java/com/cory/hourcalculator/fragments/PatchNotesFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/PatchNotesFragment.kt
@@ -86,7 +86,7 @@ class PatchNotesFragment : Fragment() {
     private fun showBugFixes() {
         bugFixesBool = !bugFixesBool
         val enhancementsChevronImage = view?.findViewById<ImageView>(R.id.bugFixesChevronImage)
-        val enhancementsArray = arrayOf(R.id.bugFixesCardView1, R.id.bugFixesCardView2, R.id.bugFixesCardView3, R.id.bugFixesCardView4)
+        val enhancementsArray = arrayOf(R.id.bugFixesCardView1, R.id.bugFixesCardView2, R.id.bugFixesCardView3, R.id.bugFixesCardView4, R.id.bugFixesCardView5)
         if (bugFixesBool) {
             for (i in 0 until enhancementsArray.count()) {
                 view?.findViewById<CardView>(enhancementsArray.elementAt(i))?.visibility = View.VISIBLE
@@ -104,7 +104,7 @@ class PatchNotesFragment : Fragment() {
     private fun showNewFeatures() {
         newFeaturesBool = !newFeaturesBool
         val newFeaturesChevronImage = view?.findViewById<ImageView>(R.id.newFeaturesChevronImage)
-        val bugFixesArray = arrayOf(R.id.newFeaturesCardView1, R.id.newFeaturesCardView2, R.id.newFeaturesCardView3, R.id.newFeaturesCardView4, R.id.newFeaturesCardView5, R.id.newFeaturesCardView6, R.id.newFeaturesCardView7, R.id.newFeaturesCardView8, R.id.newFeaturesCardView9, R.id.newFeaturesCardView10, R.id.newFeaturesCardView11, R.id.newFeaturesCardView12)
+        val bugFixesArray = arrayOf(R.id.newFeaturesCardView1, R.id.newFeaturesCardView2, R.id.newFeaturesCardView3, R.id.newFeaturesCardView4, R.id.newFeaturesCardView5, R.id.newFeaturesCardView6, R.id.newFeaturesCardView7, R.id.newFeaturesCardView8, R.id.newFeaturesCardView9, R.id.newFeaturesCardView10, R.id.newFeaturesCardView11, R.id.newFeaturesCardView12, R.id.newFeaturesCardView13, R.id.newFeaturesCardView14)
         if (newFeaturesBool) {
             for (i in 0 until bugFixesArray.count()) {
                 view?.findViewById<CardView>(bugFixesArray.elementAt(i))?.visibility = View.VISIBLE
@@ -122,7 +122,7 @@ class PatchNotesFragment : Fragment() {
     private fun showEnhancements() {
         enhancementsBool = !enhancementsBool
         val enhancementsChevronImage = view?.findViewById<ImageView>(R.id.enhancementsChevronImage)
-        val enhancementsArray = arrayOf(R.id.enhancementsCardView1, R.id.enhancementsCardView2, R.id.enhancementsCardView3, R.id.enhancementsCardView4, R.id.enhancementsCardView5, R.id.enhancementsCardView6, R.id.enhancementsCardView7, R.id.enhancementsCardView8, R.id.enhancementsCardView9, R.id.enhancementsCardView10, R.id.enhancementsCardView11, R.id.enhancementsCardView12)
+        val enhancementsArray = arrayOf(R.id.enhancementsCardView1, R.id.enhancementsCardView2, R.id.enhancementsCardView3, R.id.enhancementsCardView4, R.id.enhancementsCardView5, R.id.enhancementsCardView6, R.id.enhancementsCardView7, R.id.enhancementsCardView8, R.id.enhancementsCardView9, R.id.enhancementsCardView10, R.id.enhancementsCardView11, R.id.enhancementsCardView12, R.id.enhancementsCardView13)
         if (enhancementsBool) {
             for (i in 0 until enhancementsArray.count()) {
                 view?.findViewById<CardView>(enhancementsArray.elementAt(i))?.visibility = View.VISIBLE
