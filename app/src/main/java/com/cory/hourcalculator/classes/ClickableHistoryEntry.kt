b6c00693b33efdd0b1e8993711f27332d0a627dd
Cory Lowry <corylowry12@gmail.com>
2023-02-26 19:34:34 -0600

    Class for remembering if history item is clickable

---
+++ app/src/main/java/com/cory/hourcalculator/classes/ClickableHistoryEntry.kt
@@ -0,0 +1,21 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class ClickableHistoryEntry(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setHistoryItemClickable(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("clickable", state)
+        editor.apply()
+    }
+
+    fun loadHistoryItemClickable(): Boolean {
+        val state = sharedPreferences.getBoolean("clickable", true)
+        return (state)
+    }
+
+}
