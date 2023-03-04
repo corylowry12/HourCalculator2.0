642fc87ef7ebb3ca6024bda49a8c2ec0a21f8937
Cory Lowry <corylowry12@gmail.com>
2023-03-04 00:17:40 -0600

    Create ImageViewActivity.kt

---
+++ app/src/main/java/com/cory/hourcalculator/intents/ImageViewActivity.kt
@@ -0,0 +1,190 @@
+package com.cory.hourcalculator.intents
+
+import android.content.res.Configuration
+import android.graphics.*
+import android.media.ExifInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.cory.hourcalculator.R
+import com.cory.hourcalculator.classes.*
+import com.cory.hourcalculator.database.TimeCardDBHelper
+import com.google.android.material.appbar.MaterialToolbar
+import com.ortiz.touchview.TouchImageView
+import java.io.File
+
+class ImageViewActivity : AppCompatActivity() {
+
+    var themeSelection = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val darkThemeData = DarkThemeData(this)
+        when {
+            darkThemeData.loadDarkModeState() == 1 -> {
+                setTheme(R.style.Theme_DarkTheme)
+                themeSelection = true
+            }
+            darkThemeData.loadDarkModeState() == 0 -> {
+                setTheme(R.style.Theme_MyApplication)
+                themeSelection = false
+            }
+            darkThemeData.loadDarkModeState() == 2 -> {
+                setTheme(R.style.Theme_AMOLED)
+                themeSelection = true
+            }
+            darkThemeData.loadDarkModeState() == 3 -> {
+                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        setTheme(R.style.Theme_MyApplication)
+                        themeSelection = false
+                    }
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        setTheme(AccentColor(this).followSystemTheme(this))
+                        themeSelection = true
+                    }
+                    Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                        setTheme(R.style.Theme_AMOLED)
+                        themeSelection = true
+                    }
+                }
+            }
+        }
+        val accentColor = AccentColor(this)
+        val followSystemVersion = FollowSystemVersion(this)
+
+        when {
+            accentColor.loadAccent() == 0 -> {
+                theme!!.applyStyle(R.style.teal_accent, true)
+            }
+            accentColor.loadAccent() == 1 -> {
+                theme?.applyStyle(R.style.pink_accent, true)
+            }
+            accentColor.loadAccent() == 2 -> {
+                theme?.applyStyle(R.style.orange_accent, true)
+            }
+            accentColor.loadAccent() == 3 -> {
+                theme?.applyStyle(R.style.red_accent, true)
+            }
+            accentColor.loadAccent() == 4 -> {
+                if (!followSystemVersion.loadSystemColor()) {
+                    theme!!.applyStyle(R.style.system_accent, true)
+                }
+                else {
+                    if (themeSelection) {
+                        theme!!.applyStyle(R.style.system_accent_google, true)
+                    }
+                    else {
+                        theme?.applyStyle(R.style.system_accent_google_light, true)
+                    }
+                }
+            }
+        }
+        setContentView(R.layout.activity_image_view)
+
+        val imageView = findViewById<TouchImageView>(R.id.touchImageView)
+
+        val id = intent.getStringExtra("id")
+
+        val viewImageMaterialToolbar = findViewById<MaterialToolbar>(R.id.viewImageToolBar)
+
+        val deleteImageDrawable = viewImageMaterialToolbar.menu.findItem(R.id.deleteImage).icon
+        deleteImageDrawable!!.mutate()
+
+        val navigationDrawable = viewImageMaterialToolbar?.navigationIcon
+        navigationDrawable?.mutate()
+
+        if (MenuTintData(this).loadMenuTint()) {
+            val typedValue = TypedValue()
+            this.theme?.resolveAttribute(R.attr.historyActionBarIconTint, typedValue, true)
+            deleteImageDrawable.colorFilter = BlendModeColorFilter(ContextCompat.getColor(this, typedValue.resourceId), BlendMode.SRC_ATOP)
+            navigationDrawable?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(this, typedValue.resourceId), BlendMode.SRC_ATOP)
+        }
+        else {
+            val typedValue = TypedValue()
+            this.theme?.resolveAttribute(R.attr.textColor, typedValue, true)
+            deleteImageDrawable?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(this, typedValue.resourceId), BlendMode.SRC_ATOP)
+            navigationDrawable?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(this, typedValue.resourceId), BlendMode.SRC_ATOP)
+        }
+
+        viewImageMaterialToolbar.setNavigationOnClickListener {
+            Vibrate().vibration(this)
+            imageView.resetZoom()
+            finishAfterTransition()
+        }
+        viewImageMaterialToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.deleteImage -> {
+                    Vibrate().vibration(this)
+                    TimeCardDBHelper(this, null).addImage(id!!, "")
+                    finish()
+                    Toast.makeText(this, "Image Deleted", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+                else -> false
+            }
+        }
+
+        try {
+
+            val imageCursor = TimeCardDBHelper(this, null).getImage(id!!)
+            imageCursor.moveToFirst()
+
+            val imageMap = HashMap<String, String>()
+            imageMap["image"] =
+                imageCursor.getString(imageCursor.getColumnIndex(TimeCardDBHelper.COLUMN_IMAGE))
+
+            val bitmapSrc = BitmapFactory.decodeFile(imageMap["image"])
+
+            val ei = ExifInterface(imageMap["image"]!!)
+
+            val orientation = ei.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+
+            val m = Matrix()
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> {
+                    m.postRotate(90f)
+                }
+                ExifInterface.ORIENTATION_ROTATE_180 -> {
+                    m.postRotate(180f)
+                }
+                ExifInterface.ORIENTATION_ROTATE_270 -> {
+                    m.postRotate(270f)
+                }
+            }
+            val bitmapScaled = Bitmap.createScaledBitmap(bitmapSrc, bitmapSrc.width, bitmapSrc.height, true)
+            val bitmap =
+                Bitmap.createBitmap(
+                    bitmapScaled,
+                    0,
+                    0,
+                    bitmapScaled.width,
+                    bitmapScaled.height,
+                    m,
+                    true
+                )
+            imageView.setImageBitmap(bitmap)
+
+        } catch (e : java.lang.NullPointerException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                //imageView.setZoom(1f)
+                imageView.resetZoom()
+                finishAfterTransition()
+            }
+        })
+    }
+}
