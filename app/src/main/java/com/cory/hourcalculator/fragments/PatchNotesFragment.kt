594f78804fb95c268d36059d8e2990eec38bdd67
Cory Lowry <corylowry12@gmail.com>
2023-03-04 00:19:02 -0600

    Update PatchNotesFragment.kt

--- app/src/main/java/com/cory/hourcalculator/fragments/PatchNotesFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/PatchNotesFragment.kt
@@ -1,21 +1,22 @@
 package com.cory.hourcalculator.fragments
 
 import android.content.res.Configuration
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cory.hourcalculator.BuildConfig
-import com.cory.hourcalculator.MainActivity
+import com.cory.hourcalculator.intents.MainActivity
 import com.cory.hourcalculator.R
 import com.cory.hourcalculator.adapters.PatchNotesBugFixesAdapter
 import com.cory.hourcalculator.adapters.PatchNotesEnhancementsAdapter
@@ -32,10 +33,10 @@ class PatchNotesFragment : Fragment() {
     private var bugFixesArray = arrayOf("Fixed issue where when resetting \'App Settings\' to default it would uncheck the Vibration switch even though vibration is enabled after resetting", "Fixed issue with there being no vibration when long clicking items in history view",
                                         "Fixed issue with there being no vibration when clicking \"Hide\" to hide checkboxes", "Fixed issue with there being no vibration when clicking \"Report A Bug\" in the about app view", "Fixed issue with the shape of the floating action button in the history view",
                                         "Fixed crashing when trying to leave the edit hour view if there were pending changes and you clicked yes to save them", "Fixed issue with title and menu buttons not being centered properly in title bars",
-                                        "Fixed issue with github button in about view still using built in web view instead of custom tab")
+                                        "Fixed issue with github button in about view still using built in web view instead of custom tab", "Fixed issue when hitting back button and the bottom nav bar wouldn't change selected tab")
 
     private var newFeaturesArray = arrayOf("Added the ability to click on the history tab when the history view is active and scroll to the top", "Added the ability to calculate in time or decimal format depending on which is enabled by long pressing the calculate button (eg. if decimal format is enabled, long pressing will calculate in time format)",
-                                            "Added the ability to click on an hour entry and open it to edit it (disabled by default, to enable go to Settings->App Settings->Open hour for editing on history item click)")
+                                            "Added the ability to click on an hour entry and open it to edit it (disabled by default, to enable go to Settings->App Settings->Open hour for editing on history item click)", "Added a toggle to change menu item tint to match the theme")
 
     private var enhancementsArray = arrayOf("Enabling and disabling history will now use a switch instead of radio buttons", "The switches in the settings will now have an icon based on their value", "Converted dialogs throughout the app to be Bottom Sheets",
                                             "Made the entire view clickable when selecting an app icon instead of just the check box", "Made the entire view clickable when selecting multiple hours to delete, it will function the same way as the checkbox (clicking will select and long clicking will select all)",
@@ -43,13 +44,18 @@ class PatchNotesFragment : Fragment() {
                                             "Optimizations when launching the app", "Optimizations in switching tabs", "Improved the quality of the app icon logos in the Appearance Settings view", "Status bar will now match the color of the theme and will change color based on scroll position",
                                             "App icon radio buttons will no be checked if you click the icon but not the radio button", "App will no longer let you go to another tab when edit view is visible to prevent accidentally leaving and losing edited data",
                                             "Adjusted left and right margins for the output text view on the home view", "App will now only use custom tabs instead of the previous built in web view", "Custom tabs will now match the accent color of the app", "Changed red 1 logo when there was a new update in patch notes setting item",
-                                            "Updated themed icon to match the other regular icons")
+                                            "Updated themed icon to match the other regular icons", "Changed the chip color in the patch notes and time cards view to match the theming better", "Delete menu item in the edit view is now an icon instead of a drop down menu")
 
-    private var bugFixesArrayInternal = arrayOf("No new features")
+    private var bugFixesArrayInternal = arrayOf("Fixed issue where if history tab was active and you clicked the history tab to scroll to the top it would still try to scroll to the top even if you already were", "Fixed issue when hitting back button and the bottom nav bar wouldn't change selected tab", "Fixed issue where if you selected a different icon in the Appearance settings, it would create 2 icons under certain conditions",
+                                                "Fixed issue where the text in the time cards view might get cut off by the item count chip in each entry", "Fixed camera not launching on some devices when trying to take a photo in Time Card Info view", "Fixed issue where if you exported entries multiple times in a row in quick succession, it would combine the entries (eg if you exported 1 hour 3 times, it would have 1 time card entry with 3 days listed)",
+                                                "Fixed issue with app crashing when taking a picture and then rotating the device on occasion", "Fixed issue where if you went to Settings->History Settings and then selected the history tab, the history view would not open", "Fixed some issues with the wrong animation showing if you switched views using the back button and then selected another view in the bottom nav bar under certain conditions",
+                                                "Fixed issue with new tab not selected if exiting time card view with back button", "Fixed issue with janky animation when deleting image in Time Card Info view", "Fixed issue with crashing when exporting hours in time format")
 
-    private var newFeaturesArrayInternal = arrayOf("No new features")
+    private var newFeaturesArrayInternal = arrayOf("Added a toggle to change menu item tint to match the theme", "Added the ability to store time card images", "Added the ability to delete time card images", "Added the ability to click on the time card tab when the view is already active and scroll to the top (the same way the history tab does)")
 
-    private var enhancementsArrayInternal = arrayOf("Updated themed icon to match the other regular icons")
+    private var enhancementsArrayInternal = arrayOf("Updated themed icon to match the other regular icons", "Changed the chip color in the patch notes and time cards view to match the theming better", "App will now enable the Material You icon when Auto icon theming is enabled (Android 13+ only)", "App will now show decimal and time total hours at the same time in History->Info", "Added a button to scroll to the top in the time cards view",
+                                                    "Delete menu item in the edit view is now an icon instead of a drop down menu", "Now when exporting entries, it will say eg 03/03/3023 instead of 03/03/2023-03/03/2023 if you only export one entry", "Improved text wrapping by adding a space in the week text in the Time Card view",
+                                                    "Will now show Day instead of Week in time card view if there is only one entry for that particular time card", "Changed time cards sorting method to show most recent first", "Changed time card info sorting method to always show oldest->newest", "Adjusted bottom margins on buttons in the bottom sheets throughout the app to be more consistent", "App will now show time:minutes in the time card view")
 
     var themeSelection = false
 
@@ -126,6 +132,22 @@ class PatchNotesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val topAppBar = activity?.findViewById<MaterialToolbar>(R.id.materialToolBarPatchNotes)
+
+        val navigationDrawable = topAppBar?.navigationIcon
+        navigationDrawable?.mutate()
+
+        if (MenuTintData(requireContext()).loadMenuTint()) {
+            val typedValue = TypedValue()
+            activity?.theme?.resolveAttribute(R.attr.historyActionBarIconTint, typedValue, true)
+            val id = typedValue.resourceId
+            navigationDrawable?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(requireContext(), id), BlendMode.SRC_ATOP)
+        }
+        else {
+            val typedValue = TypedValue()
+            activity?.theme?.resolveAttribute(R.attr.textColor, typedValue, true)
+            val id = typedValue.resourceId
+            navigationDrawable?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(requireContext(), id), BlendMode.SRC_ATOP)
+        }
 
         if (BuildConfig.FLAVOR == "Internal") {
             topAppBar?.title = "Patch Notes (Internal)"
