b64941b20ffb977cf99a842b6c2fdae0cc6b1ea4
Cory Lowry <corylowry12@gmail.com>
2023-03-08 10:32:32 -0600

    Added shared pref class for colored title bar

---
+++ app/src/main/java/com/cory/hourcalculator/classes/ColoredTitleBarTextData.kt
@@ -0,0 +1,21 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class ColoredTitleBarTextData(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setTitleBarTextState(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("coloredTitleBarText", state)
+        editor.apply()
+    }
+
+    fun loadTitleBarTextState(): Boolean {
+        val state = sharedPreferences.getBoolean("coloredTitleBarText", true)
+        return (state)
+    }
+
+}
