4ba9251f6e81a541827b4326965b0fbaaa8576d2
Cory Lowry <corylowry12@gmail.com>
2023-03-19 21:30:13 -0500

    Removed unused variables

--- app/src/main/java/com/cory/hourcalculator/fragments/AboutAppFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/AboutAppFragment.kt
@@ -30,8 +30,6 @@ import com.google.android.play.core.review.ReviewManagerFactory
 
 class AboutAppFragment : Fragment() {
 
-    var themeSelection = false
-
     private var package_name = "com.android.chrome"
 
     override fun onCreateView(
@@ -42,29 +40,23 @@ class AboutAppFragment : Fragment() {
         when {
             darkThemeData.loadDarkModeState() == 1 -> {
                 activity?.setTheme(R.style.Theme_DarkTheme)
-                themeSelection = true
             }
             darkThemeData.loadDarkModeState() == 0 -> {
                 activity?.setTheme(R.style.Theme_MyApplication)
-                themeSelection = false
             }
             darkThemeData.loadDarkModeState() == 2 -> {
                 activity?.setTheme(R.style.Theme_AMOLED)
-                themeSelection = true
             }
             darkThemeData.loadDarkModeState() == 3 -> {
                 when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
                     Configuration.UI_MODE_NIGHT_NO -> {
                         activity?.setTheme(R.style.Theme_MyApplication)
-                        themeSelection = false
                     }
                     Configuration.UI_MODE_NIGHT_YES -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
-                        themeSelection = true
                     }
                     Configuration.UI_MODE_NIGHT_UNDEFINED -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
-                        themeSelection = true
                     }
                 }
             }
