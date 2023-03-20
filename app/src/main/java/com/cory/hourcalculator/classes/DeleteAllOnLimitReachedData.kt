e2e7ecbb4e743795ecb576c085ecfdc689571f5f
Cory Lowry <corylowry12@gmail.com>
2023-03-19 21:34:40 -0500

    Create DeleteAllOnLimitReachedData.kt

---
+++ app/src/main/java/com/cory/hourcalculator/classes/DeleteAllOnLimitReachedData.kt
@@ -0,0 +1,20 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class DeleteAllOnLimitReachedData(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setDeleteAllState(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("deleteAllOnLimitReached", state)
+        editor.apply()
+    }
+
+    fun loadDeleteAllState(): Boolean {
+        val state = sharedPreferences.getBoolean("deleteAllOnLimitReached", false)
+        return (state)
+    }
+}
