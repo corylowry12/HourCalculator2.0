9309eb98e77fcab88568068d668a1acc2f871674
Cory Lowry <corylowry12@gmail.com>
2023-03-19 21:34:23 -0500

    Update TimeCardSettingsFragment.kt

--- app/src/main/java/com/cory/hourcalculator/fragments/TimeCardSettingsFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/TimeCardSettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.cory.hourcalculator.fragments
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Configuration
 import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
 import android.graphics.Color
@@ -20,6 +22,8 @@ import com.cory.hourcalculator.classes.*
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.shape.CornerFamily
 import com.google.android.material.textfield.TextInputEditText
 
 class TimeCardSettingsFragment : Fragment() {
@@ -28,7 +32,33 @@ class TimeCardSettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val darkThemeData = DarkThemeData(requireContext())
+        when {
+            darkThemeData.loadDarkModeState() == 1 -> {
+                activity?.setTheme(R.style.Theme_DarkTheme)
+            }
+            darkThemeData.loadDarkModeState() == 0 -> {
+                activity?.setTheme(R.style.Theme_MyApplication)
+            }
+            darkThemeData.loadDarkModeState() == 2 -> {
+                activity?.setTheme(R.style.Theme_AMOLED)
+            }
+            darkThemeData.loadDarkModeState() == 3 -> {
+                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        activity?.setTheme(R.style.Theme_MyApplication)
+                    }
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        activity?.setTheme(
+                            R.style.Theme_AMOLED
+                        )
+                    }
+                    Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                        activity?.setTheme(R.style.Theme_AMOLED)
+                    }
+                }
+            }
+        }
         return inflater.inflate(R.layout.fragment_time_card_settings, container, false)
     }
 
@@ -41,7 +71,50 @@ class TimeCardSettingsFragment : Fragment() {
             activity?.supportFragmentManager?.popBackStack()
         }
 
+        val coloredImageViewBackgroundCardView = requireActivity().findViewById<MaterialCardView>(R.id.coloredImageViewBackgroundCardView)
+        val defaultNameCardView = requireActivity().findViewById<MaterialCardView>(R.id.cardViewDefaultName)
+        coloredImageViewBackgroundCardView.shapeAppearanceModel =
+            coloredImageViewBackgroundCardView.shapeAppearanceModel
+                .toBuilder()
+                .setTopLeftCorner(CornerFamily.ROUNDED, 28f)
+                .setTopRightCorner(CornerFamily.ROUNDED, 28f)
+                .setBottomRightCornerSize(0f)
+                .setBottomLeftCornerSize(0f)
+                .build()
+        defaultNameCardView.shapeAppearanceModel =
+            defaultNameCardView.shapeAppearanceModel
+                .toBuilder()
+                .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
+                .setTopRightCorner(CornerFamily.ROUNDED, 0f)
+                .setBottomRightCornerSize(28f)
+                .setBottomLeftCornerSize(28f)
+                .build()
+
         updateCustomColor()
+
+        val imageViewBackground = MatchImageViewContentsBackgroundData(requireContext())
+        val imageViewBackgroundSwitch = requireActivity().findViewById<MaterialSwitch>(R.id.coloredImageViewBackgroundSwitch)
+
+        if (imageViewBackground.loadMatchImageViewContents()) {
+            imageViewBackgroundSwitch.isChecked = true
+            imageViewBackgroundSwitch?.thumbIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_check_16)
+        }
+        else {
+            imageViewBackgroundSwitch.isChecked = false
+            imageViewBackgroundSwitch?.thumbIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_close_16)
+        }
+
+        coloredImageViewBackgroundCardView.setOnClickListener {
+            imageViewBackgroundSwitch.isChecked = !imageViewBackgroundSwitch.isChecked
+            imageViewBackground.setMatchImageViewContents(imageViewBackgroundSwitch.isChecked)
+
+            if (imageViewBackgroundSwitch.isChecked) {
+                imageViewBackgroundSwitch?.thumbIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_check_16)
+            }
+            else {
+                imageViewBackgroundSwitch?.thumbIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_close_16)
+            }
+        }
 
         val defaultTimeCardName = DefaultTimeCardName(requireContext())
         val defaultNameEditText = activity?.findViewById<TextInputEditText>(R.id.defaultTimeCardName)
@@ -84,6 +157,21 @@ class TimeCardSettingsFragment : Fragment() {
         collapsingToolbarLayout.setContentScrimColor(Color.parseColor(CustomColorGenerator(requireContext()).generateTopAppBarColor()))
         collapsingToolbarLayout.setStatusBarScrimColor(Color.parseColor(CustomColorGenerator(requireContext()).generateTopAppBarColor()))
         requireActivity().findViewById<MaterialCardView>(R.id.cardViewDefaultName).setCardBackgroundColor(Color.parseColor(CustomColorGenerator(requireContext()).generateCardColor()))
+        requireActivity().findViewById<MaterialCardView>(R.id.coloredImageViewBackgroundCardView).setCardBackgroundColor(Color.parseColor(CustomColorGenerator(requireContext()).generateCardColor()))
+
+        val coloredBackgroundSwitch = requireActivity().findViewById<MaterialSwitch>(R.id.coloredImageViewBackgroundSwitch)
+
+        val states = arrayOf(
+            intArrayOf(-android.R.attr.state_checked), // unchecked
+            intArrayOf(android.R.attr.state_checked)  // checked
+        )
+        val colors = intArrayOf(
+            Color.parseColor("#e7dfec"),
+            Color.parseColor(CustomColorGenerator(requireContext()).generateCustomColorPrimary())
+        )
+
+        coloredBackgroundSwitch.thumbIconTintList = ColorStateList.valueOf(Color.parseColor(CustomColorGenerator(requireContext()).generateCustomColorPrimary()))
+        coloredBackgroundSwitch.trackTintList = ColorStateList(states, colors)
 
         val topAppBar =
             requireActivity().findViewById<MaterialToolbar>(R.id.topAppBarTimeCardSettings)
