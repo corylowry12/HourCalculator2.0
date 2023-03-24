016a387cf15c8139784f461860bd35184e3de654
Cory Lowry <corylowry12@gmail.com>
2023-03-24 13:27:08 -0500

    Update UserAddedColors.kt

--- app/src/main/java/com/cory/hourcalculator/classes/UserAddedColors.kt
+++ app/src/main/java/com/cory/hourcalculator/classes/UserAddedColors.kt
@@ -2,6 +2,9 @@ package com.cory.hourcalculator.classes
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class UserAddedColors(context: Context) {
 
@@ -21,5 +24,24 @@ class UserAddedColors(context: Context) {
 
     fun clear() {
         sharedPreferences.edit().remove("userAddedColors").commit()
+    }
+
+    fun clearHash() {
+        sharedPreferences.edit().remove("userAddedColorsMap").commit()
+    }
+
+    fun insert(jsonMap: ArrayList<HashMap<String, String>>) {
+        val jsonString = Gson().toJson(jsonMap)
+        val editor = sharedPreferences.edit()
+        editor.putString("userAddedColorsMap", jsonString)
+        editor.apply()
+    }
+
+    fun read(): ArrayList<HashMap<String, String>> {
+        val defValue =
+            Gson().toJson(ArrayList<HashMap<String, List<String>>>())
+        val json = sharedPreferences.getString("userAddedColorsMap", defValue)
+        val token: TypeToken<ArrayList<HashMap<String, String>>> = object : TypeToken<ArrayList<HashMap<String, String>>>() {}
+        return Gson().fromJson(json, token.type)
     }
 }
