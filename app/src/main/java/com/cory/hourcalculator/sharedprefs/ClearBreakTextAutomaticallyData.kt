4167a0d8d7dafe90430bf38b8153ab4d5136c98d
Cory Lowry <corylowry12@gmail.com>
2023-03-30 11:16:59 -0500

    Create ClearBreakTextAutomaticallyData.kt

---
+++ app/src/main/java/com/cory/hourcalculator/sharedprefs/ClearBreakTextAutomaticallyData.kt
@@ -0,0 +1,20 @@
+package com.cory.hourcalculator.sharedprefs
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class ClearBreakTextAutomaticallyData(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setClearAutomatically(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("clearAutomatically", state)
+        editor.apply()
+    }
+
+    fun loadClearAutomatically(): Boolean {
+        val state = sharedPreferences.getBoolean("clearAutomatically", false)
+        return (state)
+    }
+}
