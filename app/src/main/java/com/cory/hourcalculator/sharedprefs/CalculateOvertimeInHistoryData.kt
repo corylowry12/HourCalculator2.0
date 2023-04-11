f98e9c04d69f25bec3d1a31eb2c69926054f6bdf
Cory Lowry <corylowry12@gmail.com>
2023-04-11 11:56:31 -0500

    Create CalculateOvertimeInHistoryData.kt

---
+++ app/src/main/java/com/cory/hourcalculator/sharedprefs/CalculateOvertimeInHistoryData.kt
@@ -0,0 +1,20 @@
+package com.cory.hourcalculator.sharedprefs
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class CalculateOvertimeInHistoryData(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setCalculateOvertimeInHistoryState(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("calculateOvertimeInHistory", state)
+        editor.apply()
+    }
+
+    fun loadCalculateOvertimeInHistoryState(): Boolean {
+        val state = sharedPreferences.getBoolean("calculateOvertimeInHistory", false)
+        return (state)
+    }
+}
