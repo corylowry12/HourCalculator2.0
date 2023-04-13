e274dbf556b845b79cb4bf6a4b43cc46d6392e77
Cory Lowry <corylowry12@gmail.com>
2023-04-13 09:57:22 -0500

    Update VibrationData.kt

--- app/src/main/java/com/cory/hourcalculator/sharedprefs/VibrationData.kt
+++ app/src/main/java/com/cory/hourcalculator/sharedprefs/VibrationData.kt
@@ -7,14 +7,25 @@ class VibrationData(context: Context) {
 
     private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
 
-    fun setVibrationState(state: Boolean) {
+    fun setVibrationOnClickState(state: Boolean) {
         val editor = sharedPreferences.edit()
         editor.putBoolean("Vibration", state)
         editor.apply()
     }
 
-    fun loadVibrationState(): Boolean {
+    fun loadVibrationOnClickState(): Boolean {
         val state = sharedPreferences.getBoolean("Vibration", true)
+        return (state)
+    }
+
+    fun setVibrationOnTimePickerChangeState(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("VibrationOnTimePickerChange", state)
+        editor.apply()
+    }
+
+    fun loadVibrationOnTimePickerChangeState(): Boolean {
+        val state = sharedPreferences.getBoolean("VibrationOnTimePickerChange", true)
         return (state)
     }
 }
