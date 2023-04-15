3f62f758774eb8b0b82d15c4a28f655d5a4b4c3a
Cory Lowry <corylowry12@gmail.com>
2023-04-15 14:55:24 -0500

    Update AppSettingsFragment.kt

--- app/src/main/java/com/cory/hourcalculator/fragments/AppSettingsFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/AppSettingsFragment.kt
@@ -1,20 +1,15 @@
 package com.cory.hourcalculator.fragments
 
-import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
 import android.graphics.Color
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.TypedValue
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -22,17 +17,26 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cory.hourcalculator.R
-import com.cory.hourcalculator.classes.*
+import com.cory.hourcalculator.classes.CustomColorGenerator
+import com.cory.hourcalculator.classes.Vibrate
 import com.cory.hourcalculator.intents.MainActivity
-import com.cory.hourcalculator.sharedprefs.*
+import com.cory.hourcalculator.sharedprefs.BreakTextBoxVisibilityData
+import com.cory.hourcalculator.sharedprefs.CalculationTypeData
+import com.cory.hourcalculator.sharedprefs.ClearBreakTextAutomaticallyData
+import com.cory.hourcalculator.sharedprefs.ClickableHistoryEntryData
+import com.cory.hourcalculator.sharedprefs.ColoredTitleBarTextData
+import com.cory.hourcalculator.sharedprefs.DarkThemeData
+import com.cory.hourcalculator.sharedprefs.HistoryToggleData
+import com.cory.hourcalculator.sharedprefs.LongPressCalculateButtonData
+import com.cory.hourcalculator.sharedprefs.MenuTintData
+import com.cory.hourcalculator.sharedprefs.OutTimeData
+import com.cory.hourcalculator.sharedprefs.ShowPatchNotesOnAppLaunchData
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.materialswitch.MaterialSwitch
 import com.google.android.material.shape.CornerFamily
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class AppSettingsFragment : Fragment() {
 
@@ -45,20 +49,25 @@ class AppSettingsFragment : Fragment() {
             darkThemeData.loadDarkModeState() == 1 -> {
                 //activity?.setTheme(R.style.Theme_DarkTheme)
             }
+
             darkThemeData.loadDarkModeState() == 0 -> {
                 activity?.setTheme(R.style.Theme_MyApplication)
             }
+
             darkThemeData.loadDarkModeState() == 2 -> {
                 activity?.setTheme(R.style.Theme_AMOLED)
             }
+
             darkThemeData.loadDarkModeState() == 3 -> {
                 when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
                     Configuration.UI_MODE_NIGHT_NO -> {
                         activity?.setTheme(R.style.Theme_MyApplication)
                     }
+
                     Configuration.UI_MODE_NIGHT_YES -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
                     }
+
                     Configuration.UI_MODE_NIGHT_UNDEFINED -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
                     }
@@ -76,20 +85,25 @@ class AppSettingsFragment : Fragment() {
             darkThemeData.loadDarkModeState() == 1 -> {
                 //activity?.setTheme(R.style.Theme_DarkTheme)
             }
+
             darkThemeData.loadDarkModeState() == 0 -> {
                 activity?.setTheme(R.style.Theme_MyApplication)
             }
+
             darkThemeData.loadDarkModeState() == 2 -> {
                 activity?.setTheme(R.style.Theme_AMOLED)
             }
+
             darkThemeData.loadDarkModeState() == 3 -> {
                 when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
                     Configuration.UI_MODE_NIGHT_NO -> {
                         activity?.setTheme(R.style.Theme_MyApplication)
                     }
+
                     Configuration.UI_MODE_NIGHT_YES -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
                     }
+
                     Configuration.UI_MODE_NIGHT_UNDEFINED -> {
                         activity?.setTheme(R.style.Theme_AMOLED)
                     }
@@ -171,6 +185,7 @@ class AppSettingsFragment : Fragment() {
                     dialog.show()
                     true
                 }
+
                 else -> true
             }
         }
@@ -212,13 +227,14 @@ class AppSettingsFragment : Fragment() {
                 .setBottomRightCornerSize(0f)
                 .setBottomLeftCornerSize(0f)
                 .build()
-        showPatchNotesOnAppLaunchCardView.shapeAppearanceModel = showPatchNotesOnAppLaunchCardView.shapeAppearanceModel
-            .toBuilder()
-            .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
-            .setTopRightCorner(CornerFamily.ROUNDED, 0f)
-            .setBottomRightCornerSize(0f)
-            .setBottomLeftCornerSize(0f)
-            .build()
+        showPatchNotesOnAppLaunchCardView.shapeAppearanceModel =
+            showPatchNotesOnAppLaunchCardView.shapeAppearanceModel
+                .toBuilder()
+                .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
+                .setTopRightCorner(CornerFamily.ROUNDED, 0f)
+                .setBottomRightCornerSize(0f)
+                .setBottomLeftCornerSize(0f)
+                .build()
         breakTextBoxCardView.shapeAppearanceModel = breakTextBoxCardView.shapeAppearanceModel
             .toBuilder()
             .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
@@ -226,13 +242,14 @@ class AppSettingsFragment : Fragment() {
             .setBottomRightCornerSize(0f)
             .setBottomLeftCornerSize(0f)
             .build()
-        clearBreakTextBoxCardView.shapeAppearanceModel = clearBreakTextBoxCardView.shapeAppearanceModel
-            .toBuilder()
-            .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
-            .setTopRightCorner(CornerFamily.ROUNDED, 0f)
-            .setBottomRightCornerSize(0f)
-            .setBottomLeftCornerSize(0f)
-            .build()
+        clearBreakTextBoxCardView.shapeAppearanceModel =
+            clearBreakTextBoxCardView.shapeAppearanceModel
+                .toBuilder()
+                .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
+                .setTopRightCorner(CornerFamily.ROUNDED, 0f)
+                .setBottomRightCornerSize(0f)
+                .setBottomLeftCornerSize(0f)
+                .build()
         vibrationCardView.shapeAppearanceModel = vibrationCardView.shapeAppearanceModel
             .toBuilder()
             .setTopLeftCorner(CornerFamily.ROUNDED, 0f)
@@ -397,7 +414,8 @@ class AppSettingsFragment : Fragment() {
         }
 
         val showPatchNotesOnAppLaunchData = ShowPatchNotesOnAppLaunchData(requireContext())
-        val showPatchNotesOnAppLaunchSwitch = requireActivity().findViewById<MaterialSwitch>(R.id.showPatchNotesOnAppLaunchSwitch)
+        val showPatchNotesOnAppLaunchSwitch =
+            requireActivity().findViewById<MaterialSwitch>(R.id.showPatchNotesOnAppLaunchSwitch)
 
         if (showPatchNotesOnAppLaunchData.loadShowPatchNotesOnAppLaunch()) {
             showPatchNotesOnAppLaunchSwitch?.isChecked = true
@@ -413,7 +431,9 @@ class AppSettingsFragment : Fragment() {
         showPatchNotesOnAppLaunchCardView?.setOnClickListener {
             Vibrate().vibration(requireContext())
             showPatchNotesOnAppLaunchSwitch!!.isChecked = !showPatchNotesOnAppLaunchSwitch.isChecked
-            showPatchNotesOnAppLaunchData.setShowPatchNotesOnAppLaunch(showPatchNotesOnAppLaunchSwitch.isChecked)
+            showPatchNotesOnAppLaunchData.setShowPatchNotesOnAppLaunch(
+                showPatchNotesOnAppLaunchSwitch.isChecked
+            )
             if (showPatchNotesOnAppLaunchSwitch.isChecked) {
                 showPatchNotesOnAppLaunchSwitch.thumbIconDrawable =
                     ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_check_16)
@@ -499,7 +519,8 @@ class AppSettingsFragment : Fragment() {
         }
 
         val clearBreakTextAutomaticallyData = ClearBreakTextAutomaticallyData(requireContext())
-        val clearBreakTextBoxSwitch = requireActivity().findViewById<MaterialSwitch>(R.id.clearBreakTextBoxSwitch)
+        val clearBreakTextBoxSwitch =
+            requireActivity().findViewById<MaterialSwitch>(R.id.clearBreakTextBoxSwitch)
 
         if (clearBreakTextAutomaticallyData.loadClearAutomatically()) {
             clearBreakTextBoxSwitch.thumbIconDrawable =
@@ -558,7 +579,8 @@ class AppSettingsFragment : Fragment() {
                 R.anim.enter_from_left,
                 R.anim.exit_to_right
             )
-            transaction?.replace(R.id.fragment_container, VibrationSettingsFragment())?.addToBackStack(null)
+            transaction?.replace(R.id.fragment_container, VibrationSettingsFragment())
+                ?.addToBackStack(null)
             transaction?.commit()
         }
     }
@@ -570,12 +592,10 @@ class AppSettingsFragment : Fragment() {
         val vibrationSwitch = view?.findViewById<MaterialSwitch>(R.id.vibrationSwitch)
         val toggleHistory = view?.findViewById<MaterialSwitch>(R.id.historySwitch)
         val toggleBreakTextBox = view?.findViewById<MaterialSwitch>(R.id.breakTextBoxSwitch)
-        //val wagesEditText = view?.findViewById<TextInputEditText>(R.id.Wages)
 
         CalculationTypeData(requireContext()).setCalculationState(true)
         HistoryToggleData(requireContext()).setHistoryToggle(true)
         BreakTextBoxVisibilityData(requireContext()).setVisibility(0)
-        //WagesData(requireContext()).setWageAmount(getString(R.string.wages_default))
         OutTimeData(requireContext()).setOutTimeState(true)
         ClickableHistoryEntryData(requireContext()).setHistoryItemClickable(true)
 
@@ -584,7 +604,6 @@ class AppSettingsFragment : Fragment() {
         vibrationSwitch?.isChecked = true
         toggleHistory?.isChecked = true
         toggleBreakTextBox?.isChecked = true
-        //wagesEditText?.setText(getString(R.string.wages_default))
 
         setOutTimeSwitch?.thumbIconDrawable =
             ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_check_16)
@@ -605,7 +624,7 @@ class AppSettingsFragment : Fragment() {
 
         Toast.makeText(
             requireContext(),
-            getString(R.string.app_settings_reset_to_default),
+            getString(R.string.reset_to_default),
             Toast.LENGTH_LONG
         )
             .show()
@@ -688,7 +707,8 @@ class AppSettingsFragment : Fragment() {
             requireActivity().findViewById<MaterialSwitch>(R.id.setCalculationTypeSwitch)
         val toggleLongPressingCalculateSwitch =
             requireActivity().findViewById<MaterialSwitch>(R.id.toggleLongPressingCalculateSwitch)
-        val showPatchNotesOnAppLaunchSwitch = requireActivity().findViewById<MaterialSwitch>(R.id.showPatchNotesOnAppLaunchSwitch)
+        val showPatchNotesOnAppLaunchSwitch =
+            requireActivity().findViewById<MaterialSwitch>(R.id.showPatchNotesOnAppLaunchSwitch)
         val breakTextBoxSwitch =
             requireActivity().findViewById<MaterialSwitch>(R.id.breakTextBoxSwitch)
         val clearBreakTextBoxSwitch =
