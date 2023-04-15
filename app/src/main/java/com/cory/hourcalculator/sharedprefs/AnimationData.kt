edc6024303a8f35bb2ac30c52f9b00ab2176c45c
Cory Lowry <corylowry12@gmail.com>
2023-04-15 14:59:35 -0500

    Create AnimationData.kt

---
+++ app/src/main/java/com/cory/hourcalculator/sharedprefs/AnimationData.kt
@@ -0,0 +1,64 @@
+package com.cory.hourcalculator.sharedprefs
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class AnimationData(context: Context) {
+
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
+
+    fun setHistoryRecyclerViewLoadingAnimation(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("historyRecyclerViewLoadingAnimation", state)
+        editor.apply()
+    }
+
+    fun loadHistoryRecyclerViewLoadingAnimation(): Boolean {
+        val state = sharedPreferences.getBoolean("historyRecyclerViewLoadingAnimation", false)
+        return (state)
+    }
+
+    fun setTimeCardRecyclerViewLoadingAnimation(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("timeCardRecyclerViewLoadingAnimation", state)
+        editor.apply()
+    }
+
+    fun loadTimeCardRecyclerViewLoadingAnimation(): Boolean {
+        val state = sharedPreferences.getBoolean("timeCardRecyclerViewLoadingAnimation", false)
+        return (state)
+    }
+
+    fun setHistoryScrollingLoadingAnimation(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("historyScrollingAnimation", state)
+        editor.apply()
+    }
+
+    fun loadHistoryScrollingAnimation(): Boolean {
+        val state = sharedPreferences.getBoolean("historyScrollingAnimation", false)
+        return (state)
+    }
+
+    fun setTimeCardsScrollingLoadingAnimation(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("timeCardsScrollingAnimation", state)
+        editor.apply()
+    }
+
+    fun loadTimeCardsScrollingAnimation(): Boolean {
+        val state = sharedPreferences.getBoolean("timeCardsScrollingAnimation", false)
+        return (state)
+    }
+
+    fun setImageAnimation(state: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("imageOpening/ClosingAnimation", state)
+        editor.apply()
+    }
+
+    fun loadImageAnimation(): Boolean {
+        val state = sharedPreferences.getBoolean("imageOpening/ClosingAnimation", true)
+        return (state)
+    }
+}
