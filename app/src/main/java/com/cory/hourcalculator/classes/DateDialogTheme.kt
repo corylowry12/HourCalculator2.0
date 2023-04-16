1c2a3c910e14f266800155ad751b8afa185bfd4a
Cory Lowry <corylowry12@gmail.com>
2023-04-16 14:07:21 -0500

    Create DateDialogTheme.kt

---
+++ app/src/main/java/com/cory/hourcalculator/classes/DateDialogTheme.kt
@@ -0,0 +1,38 @@
+package com.cory.hourcalculator.classes
+
+import android.content.Context
+import android.content.res.Configuration
+import com.cory.hourcalculator.R
+import com.cory.hourcalculator.sharedprefs.DarkThemeData
+
+class DateDialogTheme() {
+
+    fun dateDialogTheme(context: Context): Int {
+        val darkThemeData = DarkThemeData(context)
+        when (darkThemeData.loadDarkModeState()) {
+             1 -> {
+                return R.style.datePickerNew_dark
+            }
+            0 -> {
+                return R.style.datePickerNew
+            }
+            2 -> {
+                return R.style.datePickerNew_dark
+            }
+            3 -> {
+                when (context.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        return R.style.datePickerNew
+                    }
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        return R.style.datePickerNew_dark
+                    }
+                    Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                        return R.style.datePickerNew_dark
+                    }
+                }
+            }
+        }
+        return R.style.datePickerNew
+    }
+}
