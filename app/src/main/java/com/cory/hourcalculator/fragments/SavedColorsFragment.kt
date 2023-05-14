a39d12c9530e26453dead67e53800798518fc0ec
Cory Lowry <corylowry12@gmail.com>
2023-05-14 18:32:43 -0500

    Update SavedColorsFragment.kt

--- app/src/main/java/com/cory/hourcalculator/fragments/SavedColorsFragment.kt
+++ app/src/main/java/com/cory/hourcalculator/fragments/SavedColorsFragment.kt
@@ -24,6 +24,41 @@ import com.google.android.material.appbar.MaterialToolbar
 
 class SavedColorsFragment : Fragment() {
 
+    private lateinit var savedColorsAdapter: SavedColorsAdapter
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        val darkThemeData = DarkThemeData(requireContext())
+        when {
+            darkThemeData.loadDarkModeState() == 1 -> {
+                //activity?.setTheme(R.style.Theme_DarkTheme)
+            }
+            darkThemeData.loadDarkModeState() == 0 -> {
+                activity?.setTheme(R.style.Theme_MyApplication)
+            }
+            darkThemeData.loadDarkModeState() == 2 -> {
+                activity?.setTheme(R.style.Theme_AMOLED)
+            }
+            darkThemeData.loadDarkModeState() == 3 -> {
+                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        activity?.setTheme(R.style.Theme_MyApplication)
+                    }
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        activity?.setTheme(R.style.Theme_AMOLED)
+                    }
+                    Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                        activity?.setTheme(R.style.Theme_AMOLED)
+                    }
+                }
+            }
+        }
+
+        savedColorsAdapter.updateCardColor()
+        updateCustomColor()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -59,6 +94,8 @@ class SavedColorsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        savedColorsAdapter = SavedColorsAdapter(requireContext(), UserAddedColorsData(requireContext()).read(), this)
+
         val runnable = Runnable {
             (activity as MainActivity).currentTab = 3
             (activity as MainActivity).setActiveTab(3)
@@ -75,8 +112,7 @@ class SavedColorsFragment : Fragment() {
 
         val savedColorsRecyclerView = requireActivity().findViewById<RecyclerView>(R.id.savedColorRecyclerView)
         savedColorsRecyclerView?.layoutManager = LinearLayoutManager(requireContext())
-        savedColorsRecyclerView.adapter =
-            SavedColorsAdapter(requireContext(), UserAddedColorsData(requireContext()).read(), this)
+        savedColorsRecyclerView.adapter = savedColorsAdapter
     }
 
     fun updateCustomColor() {
@@ -114,6 +150,8 @@ class SavedColorsFragment : Fragment() {
             activity?.window?.navigationBarColor =
                 Color.parseColor(CustomColorGenerator(requireContext()).generateNavBarColor())
         }
+
+        savedColorsCollapsingToolbarLayout?.setExpandedTitleColor(Color.parseColor(CustomColorGenerator(requireContext()).generateTitleBarExpandedTextColor()))
 
         if (ColoredTitleBarTextData(requireContext()).loadTitleBarTextState()) {
             savedColorsCollapsingToolbarLayout?.setCollapsedTitleTextColor(Color.parseColor(CustomColorGenerator(requireContext()).generateCollapsedToolBarTextColor()))
