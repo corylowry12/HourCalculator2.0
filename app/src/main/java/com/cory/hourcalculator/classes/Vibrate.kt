8f0ef58e18a2b3023472a7371bca4136c4d30543
Cory Lowry <corylowry12@gmail.com>
2023-07-19 21:22:17 -0500

    Added another method for older Android versions

--- app/src/main/java/com/cory/hourcalculator/classes/Vibrate.kt
+++ app/src/main/java/com/cory/hourcalculator/classes/Vibrate.kt
@@ -7,106 +7,103 @@ import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.widget.Toast
 import com.cory.hourcalculator.sharedprefs.VibrationData
 
 class Vibrate {
 
     fun vibration(context: Context) {
-        val vibrationData = VibrationData(context)
+        try {
+            val vibrationData = VibrationData(context)
 
-        //val vibrator: Vibrator
-        if (vibrationData.loadVibrationOnClickState()) {
-            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val vibratorManager =
-                    context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-                vibrator = vibratorManager.defaultVibrator
-               vibrator.vibrate(
-                    VibrationEffect.createOneShot(35, VibrationEffect.DEFAULT_AMPLITUDE)
-                )
+            if (vibrationData.loadVibrationOnClickState()) {
+                val vibratorManager = context
+                    .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                val vibrator = vibratorManager.defaultVibrator
+
+                if (vibrator.hasVibrator()) {
+                    context.getSystemService(Vibrator::class.java)
+                        .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+                }
             } else {
-                vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                vibrator.vibrate(2)
-            }*/
-            val vibratorManager = context
-                .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            val vibrator = vibratorManager.defaultVibrator
-
-            if (vibrator.hasVibrator()) {
-                context.getSystemService(Vibrator::class.java)
-                    .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+                val vib = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                if (vib.hasVibrator()) {
+                    vib.vibrate(2)
+                }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
     fun vibrateOnLongClick(context: Context) {
-        val vibrationData = VibrationData(context)
+        try {
+            val vibrationData = VibrationData(context)
 
-        //val vibrator: Vibrator
-        if (vibrationData.loadVibrationOnLongClickState()) {
-            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val vibratorManager =
-                    context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-                vibrator = vibratorManager.defaultVibrator
-                vibrator.vibrate(
-                    VibrationEffect.createOneShot(35, VibrationEffect.DEFAULT_AMPLITUDE)
-                )
-            } else {
-                vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                vibrator.vibrate(2)
-            }*/
-            val vibratorManager = context
-                .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            val vibrator = vibratorManager.defaultVibrator
+            if (vibrationData.loadVibrationOnLongClickState()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    val vibratorManager = context
+                        .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                    val vibrator = vibratorManager.defaultVibrator
 
-            if (vibrator.hasVibrator()) {
-                context.getSystemService(Vibrator::class.java)
-                    .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK))
+                    if (vibrator.hasVibrator()) {
+                        context.getSystemService(Vibrator::class.java)
+                            .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK))
+                    }
+                } else {
+                    val vib = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                    if (vib.hasVibrator()) {
+                        vib.vibrate(2)
+                    }
+                }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
     fun vibrateOnError(context: Context) {
+        try {
+            val vibrationData = VibrationData(context)
 
-        val vibrationData = VibrationData(context)
+            val vib = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            if (vibrationData.loadVibrationOnErrorState()) {
 
-        val vibrator: Vibrator
-        if (vibrationData.loadVibrationOnErrorState()) {
-            val vibratorManager = context
-                .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            val vibratorCheck = vibratorManager.defaultVibrator
-
-            if (vibratorCheck.hasVibrator()) {
-                val pattern = longArrayOf(0, 50, 15, 50)
-                vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                vibrator.vibrate(pattern, -1)
+                if (vib.hasVibrator()) {
+                    val pattern = longArrayOf(0, 50, 15, 50)
+                    vib.vibrate(pattern, -1)
+                }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
     fun vibrationTimePickers(context: Context) {
-        val vibrationData = VibrationData(context)
+        try {
+            val vibrationData = VibrationData(context)
 
-        //val vibrator: Vibrator
-        if (vibrationData.loadVibrationOnTimePickerChangeState()) {
-            //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                //val vibratorManager =
-                    //context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-                //vibrator = vibratorManager.defaultVibrator
-                //vibrator.vibrate(
-                    //VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK)
-               // )
-            val vibratorManager = context
-                .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            val vibrator = vibratorManager.defaultVibrator
+            if (vibrationData.loadVibrationOnTimePickerChangeState()) {
 
-            if (vibrator.hasVibrator()) {
-                context.getSystemService(Vibrator::class.java)
-                    .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK))
+                if (Build.VERSION.SDK_INT >= 31) {
+
+                    val vibratorManager = context
+                        .getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                    val vibrator = vibratorManager.defaultVibrator
+
+                    if (vibrator.hasVibrator()) {
+                        context.getSystemService(Vibrator::class.java)
+                            .vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK))
+                    }
+                } else {
+                    val vib = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                    if (vib.hasVibrator()) {
+                        vib.vibrate(3)
+                    }
+                }
             }
-            //} else {
-               // vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                //vibrator.vibrate(1)
-           // }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
