bf67b61cd6c908901926e924358567a444dc1039
Cory Lowry <corylowry12@gmail.com>
2023-07-19 21:23:24 -0500

    Update PatchNotesChanges.kt

--- app/src/main/java/com/cory/hourcalculator/classes/PatchNotesChanges.kt
+++ app/src/main/java/com/cory/hourcalculator/classes/PatchNotesChanges.kt
@@ -3,80 +3,56 @@ package com.cory.hourcalculator.classes
 class PatchNotesChanges {
 
     var bugFixesArray = arrayOf(
-        "Fixed issue where when resetting \'App Settings\' to default it would uncheck the Vibration switch even though vibration is enabled after resetting",
-        "Fixed issue with there being no vibration when long clicking items in history view",
-        "Fixed issue with there being no vibration when clicking \"Hide\" to hide checkboxes",
-        "Fixed issue with there being no vibration when clicking \"Report A Bug\" in the about app view",
-        "Fixed issue with the shape of the floating action button in the history view",
-        "Fixed crashing when trying to leave the edit hour view if there were pending changes and you clicked yes to save them",
-        "Fixed issue with title and menu buttons not being centered properly in title bars",
-        "Fixed issue with github button in about view still using built in web view instead of custom tab",
-        "Fixed issue when hitting back button and the bottom nav bar wouldn't change selected tab",
-        "Fixed issue where if you went to edit an entry and the in time or out time hours were equal to 12, it would change it to 11",
-        "Fixed issue with deleting selected not working properly if you deleted multiple entries in quick succession one at a time",
-        "Fixed some issues with weird splash screen logos on certain devices",
-        "Fixed issue with there being not vibration when clicking the view repo button in the about app view",
-        "Fixed issue where the date would be December 31st, 1969 when undoing hour deletion in the history settings view",
-        "And so much more"
-    )
-
-    var newFeaturesArray = arrayOf(
-        "Completely new and reworked theming system that allows you to pick basically whatever color you want",
-        "Added the ability to click on the history tab when the history view is active and scroll to the top",
-        "Added the ability to calculate in time or decimal format depending on which is enabled by long pressing the calculate button (eg. if decimal format is enabled, long pressing will calculate in time format)",
-        "Added the ability to click on an hour entry and open it to edit it (disabled by default, to enable go to Settings->App Settings->Open hour for editing on history item click)",
-        "Added a toggle to change menu item tint to match the theme",
-        "Added a toggle to make badges in the navigation bar match the theme",
-        "Added a toggle where if you reach the limit you can have it automatically delete all entries instead of just the oldest one",
-        "And so much more"
-    )
-
-    var enhancementsArray = arrayOf(
-        "Enabling and disabling history will now use a switch instead of radio buttons",
-        "The switches in the settings will now have an icon based on their value",
-        "Converted dialogs throughout the app to be Bottom Sheets",
-        "Made the entire view clickable when selecting multiple hours to delete, it will function the same way as the checkbox (clicking will select and long clicking will select all)",
-        "App will now show \"Error\" if there was a problem calculating wages",
-        "App will now calculate hours via the info dialog in the history view to show HOURS:MINUTES if there is no decimal time stored",
-        "Optimizations when launching the app",
-        "Optimizations in switching tabs",
-        "Improved the quality of the app icon logos in the Appearance Settings view",
-        "Status bar will now match the color of the theme and will change color based on scroll position",
-        "App will no longer let you go to another tab when edit view is visible to prevent accidentally leaving and losing edited data",
-        "Adjusted left and right margins for the output text view on the home view",
-        "App will now only use custom tabs instead of the previous built in web view",
-        "Custom tabs will now match the accent color of the app",
-        "Changed red 1 logo when there was a new update in patch notes setting item",
-        "Updated themed icon to match the other regular icons",
-        "Changed the chip color in the patch notes and time cards view to match the theming better",
-        "Delete menu item in the edit view is now an icon instead of a drop down menu",
-        "Removed auto icon theming, you will now just have to manually pick an app icon, this way the app doesn't have to restart every time you change a theme",
-        "Major refactor of code to optimize the history view",
-        "Added a toast message when entry is automatically deleted",
-        "And so much more"
-    )
-
-    var bugFixesArrayInternal = arrayOf(
         "Fixed issue with bottom navigation bar not being colored properly when generate a random color on app launch was enabled",
-        "Fixed issue with tablet settings not showing the proper selection after the last update",
         "Fixed issue with the Time Card Scrolling Animation setting not saving",
         "Fixed issue with options bottom sheet in History not sliding up all the way on tablets",
         "Fixed issue with the bottom sheets in Time Cards not sliding up all the way on tablets",
-        "Fixed issue with the permissions bottom sheet not sliding up all the way on tablets"
+        "Fixed issue with the permissions bottom sheet not sliding up all the way on tablets",
+        "Fixed issue with crashing when the app would set a certain theme in the home view",
+        "Fixed issue with app not resetting the chosen hex color in the Accent Color view when resetting",
+        "Fixed issue with vibration causing crashing on versions of Android 11 and below",
+        "Fixed issue with app crashing upon launch due to setting menu icon colors",
+        "Fixed issue with app crashing when loading history, will now prompt to delete history",
+        "Fixed issue where app would occasionally only show one minute decimal place when calculating hours in time format",
+        "Fixed issue with app sending out a toast message saying \"called\" when clicking the History tab under certain conditions",
+        "Fixed issue with bottom navigation bar not updating in some cases",
+        "Fixed issue with crashing when opening app settings",
+        "Fixed issue with crashing when opening history settings"
+    )
+
+    var newFeaturesArray = arrayOf(
+        "Added support for tablets",
+        "Added tablet settings for tablets",
+        "Added options to reposition items in the navigation rail for tablets",
+        "Added the option to set the History FAB to be positioned on the left, center, or right",
+        "App will no longer show the take a photo button if the device does not have a camera",
+        "Disabled colored menu tint settings on Android versions less than 10"
+    )
+
+    var enhancementsArray = arrayOf(
+        "Added support for auto rotation for tablets",
+        "Vibration settings will no longer be shown in App Settings if the device doesn't support vibration",
+        "History will now be two columns on tablets and one column on smart phones",
+        "Time Cards will now be two columns on tablets and one column on smart phones",
+        "Added error checking that will now prompt you to delete history if there is an error loading items",
+        "App will no longer show the take a photo button if the device does not have a camera"
+    )
+
+    var bugFixesArrayInternal = arrayOf(
+        "Fixed issue with app sending out a toast message saying \"called\" when clicking the History tab under certain conditions",
+        "Fixed issue with bottom navigation bar not updating in some cases",
+        "Fixed issue with crashing when opening app settings",
+        "Fixed issue with crashing when opening history settings"
     )
 
     var newFeaturesArrayInternal = arrayOf(
-        "Added support for tablets",
-        "Added tablet settings for tablets",
-        "Added options to reposition items in the navigation rail for tablets"
+        "Added the option to set the History FAB to be positioned on the left, center, or right",
+        "Added the option to set the Time Card FAB to be positioned on the left, center, or right"
     )
 
     var enhancementsArrayInternal = arrayOf(
-        "Added support for auto rotation",
-        "Vibration settings will no longer be shown in App Settings if the device doesn't support vibration",
-        "Disabled auto rotate on devices that aren't tablets",
-        "History will now be two columns on tablets and one column on smart phones",
-        "Time Cards will now be two columns on tablets and one column on smart phones"
+        "App will no longer show the take a photo button if the device does not have a camera",
+        "Disabled colored menu tint settings on Android versions less than 10"
     )
 
 
